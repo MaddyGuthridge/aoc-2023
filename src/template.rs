1d0bb7c669bfd364ce2b87e21b867107e1ba3726
Miguel Guthridge <hello@miguelguthridge.com>
2023-12-19 22:18:17 +1100

    Add template file

---
+++ src/template.rs
@@ -0,0 +1,35 @@
+
+#[aoc(day0, part1)]
+pub fn part_1(input: &str) -> usize {
+    todo!()
+}
+
+#[aoc(day0, part2)]
+pub fn part_2(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::{part_1, part_2};
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(
+                ""
+            ),
+            0
+        )
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(
+                ""
+            ),
+            0
+        )
+    }
+}
